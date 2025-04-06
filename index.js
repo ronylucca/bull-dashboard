eafd0b52c36de285430be83abc558283b050a388
Rony de Lucca <ronylucca@gmail.com>
2025-04-06 18:12:52 -0400

    queue list updated

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const { ExpressAdapter } = require('@bull-board/express');
 //   redis: { port: 6379, host: '127.0.0.1' },
 // }); // if you have a special connection to redis.
 const ticketQueue = new Queue('ticket');
-
+const changeQueue = new Queue('change');
 const serverAdapter = new ExpressAdapter();
 
 serverAdapter.setBasePath('/admin/queues');
 
 const { addQueue, removeQueue, setQueues, replaceQueues } = createBullBoard({
-  queues: [new BullAdapter(ticketQueue)],
+  queues: [new BullAdapter(ticketQueue), new BullAdapter(changeQueue)],
   serverAdapter: serverAdapter,
 });
 
@@ -25,8 +25,8 @@ app.use('/admin/queues', serverAdapter.getRouter());
 
 // other configurations of your server
 
-app.listen(3000, () => {
-  console.log('Running on 3000...');
-  console.log('For the UI, open http://localhost:3000/admin/queues');
+app.listen(3010, () => {
+  console.log('Running on 3010...');
+  console.log('For the UI, open http://localhost:3010/admin/queues');
   console.log('Make sure Redis is running on port 6379 by default');
 });
